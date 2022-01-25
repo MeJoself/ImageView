94bd9a65d6aefa6ed395f2383b8d80692471bc43
Jose Faustino <josefaustino.channel@gmail.com>
2022-01-25 13:40:18 -0700

    Finish product

--- ImageView/ViewController.swift
+++ ImageView/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MyImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onButtonTap(_ sender: Any) {
+        let myImage = UIImage(named: "The Batman")
+        MyImageView.image = myImage
+        
+    }
+    
 }
 
